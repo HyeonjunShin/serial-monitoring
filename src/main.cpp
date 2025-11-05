5ffefa7419bc48cec0d1090785c7ab7e6034e64c
HyeonjunShin <hyeonnnjnnn@gmail.com>
2025-11-05 16:05:27 +0900

    DEV: Applied the config.json file read to set the communication information.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "serial_reader.hpp"
 // #include "DataParser.hpp"
 // #include "CsvWriter.hpp"
+#include "config.hpp"
 #include <boost/asio.hpp>
 #include <cstddef>
 #include <cstdint>
@@ -10,27 +11,6 @@
 #include <optional>
 #include <pthread.h>
 #include <thread>
-
-const std::string PORT_ = "/dev/ttyUSB0";
-constexpr int BAUD_RATE = 921600;
-
-constexpr uint8_t HEADER[4] = {0xFF, 0xFF, 0xFD, 0x00};
-
-#pragma pack(push, 1)
-struct DataPacket {
-  uint8_t header[4];
-  uint16_t data_size;
-  uint32_t tick;
-  bool flag;
-  int16_t acc[3];
-  int16_t gyro[3];
-  int16_t temperature;
-  uint16_t loadcell;
-  uint16_t mic[32];
-  uint16_t crc;
-};
-#pragma pack(pop)
-constexpr int PACKET_SIZE = sizeof(DataPacket);
 
 std::ostream &operator<<(std::ostream &os, const DataPacket &packet) {
   os << "--- DataPacket Info ---" << std::endl;
@@ -56,8 +36,9 @@ std::ostream &operator<<(std::ostream &os, const DataPacket &packet) {
   os << "Loadcell Value: " << packet.loadcell << std::endl;
 
   os << "Mic Data (first 5): [";
-  for (int i = 0; i < 5 && i < 32; ++i) {
-    os << packet.mic[i] << (i == 4 ? "" : ", ");
+  for (int i = 0; i < 32; ++i) {
+    // os << packet.mic[i] << (i == 4 ? "" : ", ");
+    os << packet.mic[i] << ", ";
   }
   os << ", ...]" << std::endl;
 
@@ -67,67 +48,42 @@ std::ostream &operator<<(std::ostream &os, const DataPacket &packet) {
   return os;
 }
 
-static const uint16_t CRC_TABLE[256] = {
-    0x0000, 0x8005, 0x800F, 0x000A, 0x801B, 0x001E, 0x0014, 0x8011, 0x8033,
-    0x0036, 0x003C, 0x8039, 0x0028, 0x802D, 0x8027, 0x0022, 0x8063, 0x0066,
-    0x006C, 0x8069, 0x0078, 0x807D, 0x8077, 0x0072, 0x0050, 0x8055, 0x805F,
-    0x005A, 0x804B, 0x004E, 0x0044, 0x8041, 0x80C3, 0x00C6, 0x00CC, 0x80C9,
-    0x00D8, 0x80DD, 0x80D7, 0x00D2, 0x00F0, 0x80F5, 0x80FF, 0x00FA, 0x80EB,
-    0x00EE, 0x00E4, 0x80E1, 0x00A0, 0x80A5, 0x80AF, 0x00AA, 0x80BB, 0x00BE,
-    0x00B4, 0x80B1, 0x8093, 0x0096, 0x009C, 0x8099, 0x0088, 0x808D, 0x8087,
-    0x0082, 0x8183, 0x0186, 0x018C, 0x8189, 0x0198, 0x819D, 0x8197, 0x0192,
-    0x01B0, 0x81B5, 0x81BF, 0x01BA, 0x81AB, 0x01AE, 0x01A4, 0x81A1, 0x01E0,
-    0x81E5, 0x81EF, 0x01EA, 0x81FB, 0x01FE, 0x01F4, 0x81F1, 0x81D3, 0x01D6,
-    0x01DC, 0x81D9, 0x01C8, 0x81CD, 0x81C7, 0x01C2, 0x0140, 0x8145, 0x814F,
-    0x014A, 0x815B, 0x015E, 0x0154, 0x8151, 0x8173, 0x0176, 0x017C, 0x8179,
-    0x0168, 0x816D, 0x8167, 0x0162, 0x8123, 0x0126, 0x012C, 0x8129, 0x0138,
-    0x813D, 0x8137, 0x0132, 0x0110, 0x8115, 0x811F, 0x011A, 0x810B, 0x010E,
-    0x0104, 0x8101, 0x8303, 0x0306, 0x030C, 0x8309, 0x0318, 0x831D, 0x8317,
-    0x0312, 0x0330, 0x8335, 0x833F, 0x033A, 0x832B, 0x032E, 0x0324, 0x8321,
-    0x0360, 0x8365, 0x836F, 0x036A, 0x837B, 0x037E, 0x0374, 0x8371, 0x8353,
-    0x0356, 0x035C, 0x8359, 0x0348, 0x834D, 0x8347, 0x0342, 0x03C0, 0x83C5,
-    0x83CF, 0x03CA, 0x83DB, 0x03DE, 0x03D4, 0x83D1, 0x83F3, 0x03F6, 0x03FC,
-    0x83F9, 0x03E8, 0x83ED, 0x83E7, 0x03E2, 0x83A3, 0x03A6, 0x03AC, 0x83A9,
-    0x03B8, 0x83BD, 0x83B7, 0x03B2, 0x0390, 0x8395, 0x839F, 0x039A, 0x838B,
-    0x038E, 0x0384, 0x8381, 0x0280, 0x8285, 0x828F, 0x028A, 0x829B, 0x029E,
-    0x0294, 0x8291, 0x82B3, 0x02B6, 0x02BC, 0x82B9, 0x02A8, 0x82AD, 0x82A7,
-    0x02A2, 0x82E3, 0x02E6, 0x02EC, 0x82E9, 0x02F8, 0x82FD, 0x82F7, 0x02F2,
-    0x02D0, 0x82D5, 0x82DF, 0x02DA, 0x82CB, 0x02CE, 0x02C4, 0x82C1, 0x8243,
-    0x0246, 0x024C, 0x8249, 0x0258, 0x825D, 0x8257, 0x0252, 0x0270, 0x8275,
-    0x827F, 0x027A, 0x826B, 0x026E, 0x0264, 0x8261, 0x0220, 0x8225, 0x822F,
-    0x022A, 0x823B, 0x023E, 0x0234, 0x8231, 0x8213, 0x0216, 0x021C, 0x8219,
-    0x0208, 0x820D, 0x8207, 0x0202};
-
-uint16_t calcCRC(const uint8_t *data, size_t length) {
+uint16_t calcCRC(const uint8_t *data, size_t length,
+                 const std::vector<uint16_t> &crc_table) {
   uint16_t crc = 0;
   for (size_t i = 0; i < length; ++i) {
     uint8_t idx = ((crc >> 8) ^ data[i]) & 0xFF;
-    crc = ((crc << 8) ^ CRC_TABLE[idx]) & 0xFFFF;
+    crc = ((crc << 8) ^ crc_table[idx]) & 0xFFFF;
   }
   return crc;
 }
 
 int main() {
+  Config config;
+  try {
+    config = load_config("/workspaces/serial-monitoring/config.json");
+  } catch (const std::exception &e) {
+    std::cerr << "Error loading config: " << e.what() << std::endl;
+    return 1;
+  }
+
+  const std::string PORT = config.serial_config.port;
+  const int BAUD_RATE = config.serial_config.baud_rate;
+  const std::vector<uint8_t> HEADER = config.packet_format.header;
+  const std::vector<uint16_t> CRC_TABLE = config.packet_format.crc_table;
+  const std::vector<PacketField> PACKET_STRUCTURE = config.packet_structure;
+
   boost::asio::io_context io_;
   boost::asio::serial_port serial(io_);
 
   try {
-    serial.open(PORT_);
+    serial.open(PORT);
     serial.set_option(boost::asio::serial_port_base::baud_rate(BAUD_RATE));
     serial.set_option(boost::asio::serial_port_base::character_size(8));
     serial.set_option(boost::asio::serial_port_base::stop_bits(
         boost::asio::serial_port_base::stop_bits::one));
     serial.set_option(boost::asio::serial_port_base::parity(
         boost::asio::serial_port_base::parity::none));
-
-    //   serial_.open(port_);
-    //   serial_.set_option(boost::asio::serial_port_base::baud_rate(baud_rate_));
-    //   serial_.set_option(boost::asio::serial_port_base::character_size(8));
-    //   serial_.set_option(boost::asio::serial_port_base::stop_bits(
-    //       boost::asio::serial_port_base::stop_bits::one));
-    //   serial_.set_option(boost::asio::serial_port_base::parity(
-    //       boost::asio::serial_port_base::parity::none));
-    //   return true;
   } catch (const std::exception &e) {
     std::cerr << "Serial open error: " << e.what() << std::endl;
     return 1;
@@ -147,11 +103,11 @@ int main() {
 
     for (size_t i = 0; i + PACKET_SIZE <= buffer.size(); ++i) {
       // byte stream의 길이가 packet을 decoding 할 수 있는만큼 쌓였는가를 확인
-      if (memcmp(&buffer[i], HEADER, 4) == 0) {
+      if (memcmp(&buffer[i], HEADER.data(), 4) == 0) {
         DataPacket packet_vaild;
         memcpy(&packet_vaild, &buffer[i], PACKET_SIZE);
-        if (calcCRC(reinterpret_cast<uint8_t *>(&packet_vaild),
-                    PACKET_SIZE - 2) == packet_vaild.crc) {
+        if (calcCRC(reinterpret_cast<uint8_t *>(&packet_vaild), PACKET_SIZE - 2,
+                    CRC_TABLE) == packet_vaild.crc) {
           buffer.erase(buffer.begin(), buffer.begin() + i + PACKET_SIZE);
           // return packet_vaild;
         }
@@ -164,6 +120,7 @@ int main() {
   if (serial.is_open()) {
     serial.close();
   }
+
   // SerialReader serial("/dev/ttyUSB0", 921600);
   // if (!serial.open()) return 1;
 
@@ -182,7 +139,6 @@ int main() {
   //     }
   // }
 
-  // serial.close();
-  // std::cout << "Done.\n";
+  std::cout << "Done.\n";
   return 0;
 }
